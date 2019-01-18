da2d02f6e934763643e1ecbc9f3a63a724af02f2
massimoScopp <massimo.scoppettuolo@yahoo.it>
2019-01-18 11:41:12 +0100

    Mutation test con Pitest, funzionante.

--- app/src/test/java/ch/bailu/aat/coordinates/WGS84SexagesimalTest.java
+++ app/src/test/java/ch/bailu/aat/coordinates/WGS84SexagesimalTest.java
@@ -1,16 +1,17 @@
 package ch.bailu.aat.coordinates;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class WGS84SexagesimalTest {
-        WGS84Sexagesimal.Sexagesimal ws = new WGS84Sexagesimal.Sexagesimal(230.29325);
-        WGS84Sexagesimal ws1 = new WGS84Sexagesimal(25.36,186.22);
+import static org.junit.Assert.*;
+
+public class WGS84SexagesimalTest {
+    WGS84Sexagesimal.Sexagesimal ws = new WGS84Sexagesimal.Sexagesimal(230.29325);
+    WGS84Sexagesimal ws1 = new WGS84Sexagesimal(25.36,186.22);
 
     @Test
-    void getTest() {
+    public void getTest() {
         int deg = ws.getDegree();
         int sec = ws.getSecond();
         int min = ws.getMinute();
@@ -21,32 +22,32 @@ class WGS84SexagesimalTest {
     }
 
     @Test
-    void getLatitude() {
+    public void getLatitude() {
         assertEquals("25° 21' 36''",ws1.getLatitude().toString() );
     }
 
 
     @Test
-    void getLatitudeChar() {
+    public void getLatitudeChar() {
         assertEquals(78,ws1.getLatitudeChar() );
     }
 
 
     @Test
-    void toStringTest() {
+    public void toStringTest() {
         assertNotNull(ws.toString());
     }
 
     @Test
-    void getLongitude() {
+    public void getLongitude() {
         assertEquals("186° 13' 12''",ws1.getLongitude().toString());
     }
 
     @Test
-    void getLongitudeChar() {
+    public void getLongitudeChar() {
         assertEquals(69,ws1.getLongitudeChar());}
 
     @Test
-    void getLongitudeChar1() {
+    public void getLongitudeChar1() {
     }
 }
